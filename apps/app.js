8405843b5141489fb16064d006032e5d31829516
CMRWebDev <crosenberg690@gmail.com>
2015-11-19 21:51:51 -0500

    Intro CSS complete

--- apps/app.js
+++ apps/app.js
@@ -14,7 +14,7 @@ $(function() {
 		getRecent();
 	});
 
-	$('.recent-button').on("click", "#rec", function(){
+	$('.recent-button').on("click", "#recent", function(){
 		
 		// execute function that gets AJAX and displays data
 		// function takes the  tag and recent
@@ -46,7 +46,7 @@ function getRecent() {
 			cache: false,
 			url: "https://api.instagram.com/v1/tags/" + tag_name + "/media/" + search_type + "?access_token=190034554.9e869c1.ec7b0eafa1f2499b98a0d4d0823fdfc8",
   			success: function(data) {
-    			for (var i = 0; i < 6; i++) {
+    			for (var i = 0; i < 20; i++) {
       				$(".results").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
     			}
   			}
